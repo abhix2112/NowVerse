70ca3956f7a5ad6dd573337e75c7b0f98a9c8e8b
Abhishek Aggarwal <aggarwalabhi2112@gmail.com>
2025-07-02 00:29:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,32 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
-  console.log("New user connected");
+  console.log(`New user connected: ${socket.id}`);
 
   socket.on("join", (room) => {
     socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
     socket.to(room).emit("joined");
   });
 
   socket.on("offer", (data) => {
-    socket.to(data.room).emit("offer", data.sdp);
+    console.log(`Offer received from ${socket.id} for room: ${data.room}`, data);
+    socket.to(data.room).emit("offer", data);
   });
 
   socket.on("answer", (data) => {
-    socket.to(data.room).emit("answer", data.sdp);
+    console.log(`Answer received from ${socket.id} for room: ${data.room}`, data);
+    socket.to(data.room).emit("answer", data);
   });
 
   socket.on("ice-candidate", (data) => {
-    socket.to(data.room).emit("ice-candidate", data.candidate);
+    console.log(`ICE candidate received from ${socket.id} for room: ${data.room}`, data.candidate);
+    socket.to(data.room).emit("ice-candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+    socket.broadcast.emit("user-left");
   });
 });
 
